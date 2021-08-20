9e74ad28e19623a39b8c038a07a1a8a45975b77e
Alexey Medvedev <medvedevav@itzavr.ru>
2021-08-21 02:59:33 +0300

    Add checking upload file

--- example/compress.php
+++ example/compress.php
@@ -35,6 +35,10 @@ $fileCompressor = new Compressor(
     $iLovePDFProjectId . ':' . $iLovePDFProjectKey
 );
 
+if (!is_uploaded_file($_FILES['file']['name'])) {
+    jsonRequest(['data' => 'File not upload'], 500);
+}
+
 $uploadDir = __DIR__ . '/uploads/';
 $fileName = basename($_FILES['file']['name']);
 
